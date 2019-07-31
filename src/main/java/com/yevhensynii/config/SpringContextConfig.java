263051de974bd3545149ff1241446d1f16d682ee
Yevhen Synii <ibookmack@gmail.com>
2019-07-31 11:54:57 +0300

    configure spring context, added bcrypt encoding password, process autorization

---
+++ src/main/java/com/yevhensynii/config/SpringContextConfig.java
@@ -0,0 +1,52 @@
+package com.yevhensynii.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SpringContextConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("login")
+                .permitAll()
+                .successForwardUrl("/home")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .permitAll();
+    }
+
+    @Bean
+    @SuppressWarnings("unchecked")
+    public PasswordEncoder passwordEncoder() {
+        Map encoders = new HashMap<>();
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
+    }
+
+    public void configAuthUsers(AuthenticationManagerBuilder builder) throws Exception {
+        builder.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("ADMIN");
+    }
+}
